0fbab626674af71ff6cd261413fffa1863a1aa93
IGICOCORO <nduwimanaguymich@gmail.com>
2022-02-02 15:08:10 +0200

    created moto page added back photo on login

---
+++ src/views/Moto.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <button class="create">Créer</button>
+          <div class="parcelles">
+      <table>
+        <thead>
+          <tr>
+            <th>Quantite</th>
+            <th>PAU </th>
+            <th>date</th>
+            <th>Autres dépenses</th>
+            <th>Date de vente prévisionnelle </th>
+            <th>Prix de vente prévisionnelle </th>
+            <th>options</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <button class="delete"><i class="fa fa-trash"></i></button>
+              <button class="edit"><i class="fa fa-edit"></i></button>
+              <button class="read"><i class="fa fa-eye"></i></button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+  </div>
+</template>
+<script></script>
+<style>
+  
+</style>
