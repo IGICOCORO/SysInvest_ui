4fa55af36b01a523cfe98f5501b6919f711d40da
IGICOCORO <nduwimanaguymich@gmail.com>
2022-02-05 19:35:48 +0200

    made create,delete on autres investissements pages and get all data

--- src/components/dialog_autres_invest.vue
+++ src/components/dialog_autres_invest.vue
@@ -1,39 +1,139 @@
 <template>
 	<div class="popup-body">
+    <div class="overlay"></div>
+    <div class="modale">
       <div class="absolue">
-        <button class="close" @click="close">&times</button>
+        <button class="close" @click="close">X</button>
       </div>
       <center>
         <h3>Autres Investissements</h3>
       </center>
-      <form method="post">
+      <form class="form">
+         <div class="group"> 
         <div class="field">
-          <label>Type d’investissement:</label>
+          <label>Nom d’investissement:</label>
+          <input type="text" id="montant" v-model="nom_investissement">
         </div>
         <div class="field">
           <label for="montant">Montant investi:</label>
-          <input type="number" id="montant" v-model="montant" autocomplete="off" min=10000 step="10000">
+          <input type="number" id="montant" v-model="montant_investi">
         </div>
         <div class="field">
           <label>date d’investissement:</label>
+          <input type="date" id="montant" v-model="date_investissement">
         </div>
+      </div>
+       <div class="group"> 
         <div class="field">
           <label for="details">Benefice previsionnel:</label>
-          <textarea id="details" v-model="details"></textarea>
+         <input type="number" id="montant" v-model="benefice">
         </div> 
          <div class="field">
           <label for="details">Date de fin de cet investissment:</label>
-          <textarea id="details" v-model="details"></textarea>
+         <input type="date" id="montant" v-model="date_fin_investissement">
         </div>
         <div class="buttons">
-          <button type="reset">Reset</button>
-          <button type="submit">Soumettre</button>
+          <button class="reset" type="reset"  @click="annuler()">Reset</button>
+          <button  class="submit"  type="submit" @close="close" @click.prevent="submitAutresInvest()">Soumettre</button>
         </div>
+      </div>
       </form>
     </div>
+  </div>
 </template>
 <script>
-	
+	import axios from "axios"
+  export default {
+    data(){
+      return{
+        nom_investissement :null,
+        montant_investi : null,
+        date_investissement : null,
+        benefice :null,
+        date_fin_investissement :null,
+      }
+    },
+    computed:{
+    headers(){
+      return {
+        headers: {
+          "Authorization": "Bearer " + this.$store.state.user.access
+        }
+      }
+    }
+    },
+    methods: {
+      submitAutresInvest(){
+        let data = {
+          nom_investissement:this.nom_investissement,
+          montant_investi:this.montant_investi,
+          date_investissement:this.date_investissement,
+          benefice:this.benefice,
+          date_fin_investissement:this.date_fin_investissement,
+        }
+         axios.post(this.$store.state.url+'/autres_investssements/', data ,this.headers)
+         .then((response)=>{
+          this.$store.state.autres_investissements.push(response.data)
+          this.close()
+         }).catch((error) => {
+        console.log(error)
+      })
+      },
+      close() {
+        this.$emit('close');
+      }
+    },
+  };
 </script>
 <style>
+.reset{
+  background:#FF6666;
+  border-radius:30px;
+  width:100px;
+}
+.submit{
+  background:#3399FF;
+    border-radius:30px;
+    width:100px;
+}
+*{
+  justify-content:center;
+}
+.popup-body{
+  width:100%;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+}
+.overlay{
+  background:rgba(0,0,0,0.5);
+  position:fixed;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+}
+.form{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr 1fr;
+}
+.modale{
+  background:#fff;
+  height:500px;
+  width:700px;
+  color:#333;
+  padding:20px;
+  position:fixed;
+  top:20%;
+   border-radius:10px;
+   margin-left:350px;
+}
+.close{
+  background:red;
+  width:30px;
+  height:30px;
+  border-radius:10px;
+}
 </style>
--- src/views/Autres_investissements.vue
+++ src/views/Autres_investissements.vue
@@ -1,37 +1,104 @@
 <template>
-  <div class="home">
-    <button class="create">Créer</button>
-     <div class="parcelles">
-      <table>
-        <thead>
-          <tr>
-            <th>Type d’investissement</th>
-            <th>Montant investi</th>
-            <th>date d’investissement </th>
-            <th>Benefice previsionnel</th>
-            <th>Date de fin  </th>
-            <th>options</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <button class="delete"><i class="fa fa-trash"></i></button>
-              <button class="edit"><i class="fa fa-edit"></i></button>
-              <button class="read"><i class="fa fa-eye"></i></button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-  </div>
-  </div>
+    <div class="home">
+        <button class="create" @click="showModal">Créer</button>
+        <div class="parcelles">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom d’investissement</th>
+                        <th>Montant investi</th>
+                        <th>Date d’investissement </th>
+                        <th>Benefice previsionnel</th>
+                        <th>Date de fin </th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  <tr v-for=" item in autres_investissements" :key="item">
+                        <td>{{ item.nom_investissement }}</td>
+                        <td>{{ item.montant_investi }}</td>
+                        <td>{{ item.date_investissement }}</td>
+                        <td>{{ item.benefice }}</td>
+                        <td>{{ item.date_fin_investissement }}</td>
+                        <td>
+                            <button class="delete" @click="deleteInvestotre(item)"><i class="fa fa-trash"></i></button>
+                            <button class="edit"><i class="fa fa-edit"></i></button>
+                            <button class="read"><i class="fa fa-eye"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <DialogAutresInvest v-show="isModalVisible" @close="closeModal" />
+    </div>
 </template>
-<script></script>
+<script>
+import DialogAutresInvest from '../components/dialog_autres_invest.vue';
+import axios from "axios"
+export default {
+    components: {
+        DialogAutresInvest,
+    },
+    data() {
+        return {
+            nom_investissement:"",
+            montant_investi: null,
+            date_investissement: null,
+            benefice: null,
+            date_fin_investissement: null,
+            autres_investissements: [],
+            isModalVisible: false,
+            error: ''
+        }
+    },
+    computed: {
+        headers() {
+            return {
+                headers: {
+                    "Authorization": "Bearer " + this.$store.state.user.access
+                }
+            }
+        }
+    },
+    watch: {
+        "$store.state.autres_investissements"(new_val) {
+            this.autres_investissements = new_val
+        }
+    },
+    methods: {
+        fetchInvestOtres() {
+            axios.get(this.$store.state.url + '/autres_investssements/', this.headers)
+                .then((response) => {
+                    this.$store.state.autres_investissements = response.data.results
+                    console.log(response.data.results)
+                }).catch((error) => {
+                    console.log(error)
+                })
+        },
+        deleteInvestotre(item) {
+            if (confirm(`Voulez vous vraiment supprimer cet investissement ?`)) {
+                axios.delete(this.$store.state.url + `/autres_investssements/${item.id}/`,
+                        this.headers)
+                    .then((response) => {
+                        console.log(response.data)
+                        this.$store.state.autres_investissements.splice(
+                            this.$store.state.autres_investissements.indexOf(item), 1)
+                    }).catch((error) => {
+                        console.log(error)
+                    })
+            }
+        },
+        showModal() {
+            this.isModalVisible = true;
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        }
+    },
+    mounted() {
+        this.fetchInvestOtres()
+    }
+};
+</script>
 <style>
-  
 </style>
